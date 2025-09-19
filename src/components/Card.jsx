93efb52d620efa7c92782e889324c116b8b715eb
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-09-19 17:07:22 +0530

    Initialize project with Vite, React, and Tailwind CSS setup; add main application structure and components.

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+export const Card = () => {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline text-blue-700">
+          Hello world!
+        </h1>
+      </div>
+      <div>
+        <button className="bg-blue-500 hover:bg-blue-700 text-green font-bold py-2 px-4 rounded">
+          Button
+        </button>
+      </div>
+      <h1 class="text-3xl  underline">Hello world!</h1>
+    </>
+  );
+};
